d3c95a3705b975618e9224a29927eed8f9a2c801
chimalexoficial <you@example.com>
2022-11-18 19:02:54 -0800

    finished

--- src/App.js
+++ src/App.js
@@ -3,37 +3,52 @@ import Loading from './Loading'
 import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => id !== tour.id);
+    setTours(newTours);
+  }
+
   const fetchTours = async () => {
+    setLoading(true);
     try {
       const response = await fetch(url);
       const tours = await response.json();
       setLoading(false);
       setTours(tours);
-      //console.log(tours);
     } catch (error) {
       setLoading(false);
-      console.log(error);
     }
   }
 
   useEffect(() => {
     fetchTours();
-  }, [])
+  }, []);
 
   if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
     return <main>
-      <Loading />
+      <div className='title'>
+        <h2>no tours left</h2>
+        <button className='btn' onClick={fetchTours}>refresh</button>
+      </div>
     </main>
   }
 
   return <main>
-    <Tours />
+    <Tours tours={tours} removeTour={removeTour} />
   </main>
 }
 
